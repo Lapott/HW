5f075a833adbd5b6a572798e60b5d6500e502237
Lapott <layshev1970@gmail.com>
2024-05-26 00:41:26 +0300

    @alsigna Function Task6 Sorted

---
+++ Function Task6 Sorted.py
@@ -0,0 +1,16 @@
+devices = {
+    "rt3": {
+        "nb_id": 32,
+        "ip": "3.3.3.3",
+    },
+    "rt1": {
+        "nb_id": 908,
+        "ip": "1.1.1.1",
+    },
+    "sw2": {
+        "nb_id": 5233,
+        "ip": "2.2.2.2",
+    },
+}
+
+dict(sorted(devices.items(), key=lambda x: x[1]["nb_id"]))
